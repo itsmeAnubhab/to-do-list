c02f50e7af5768e2dc6a5fe17096e5096454b58e
itsmeAnubhab <91974827+itsmeAnubhab@users.noreply.github.com>
2022-03-04 01:34:45 +0530

    updated to do list

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
-let inputfield=document.getElementById('inputfield');
-let addtodobtn=document.getElementById('addtodo');
-let todocontainer=document.getElementById('todocontainer');
+const express = require("express");
+const bodyparser=require("body-parser");
 
-addtodobtn.addEventListener('click',function(){
-var paragraph=document.createElement('p');
-var dltbtn=document.createElement("button");
-paragraph.innerText = inputfield.value;
-dltbtn.innerText = "Delete";
-todocontainer.appendChild(paragraph);
-todocontainer.appendChild(dltbtn);
-inputfield.value = "";
+const app=express();
+var items=[];
+app.set("view engine","ejs");
+app.use(express.static("public"))
+app.use(bodyparser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+    var date="monday";
+    var today=new Date();
+    var options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+    var day= today.toLocaleDateString("en-US",options);
 
-
-
-dltbtn.addEventListener('click', function(){
-    todocontainer.removeChild(paragraph);
-    todocontainer.removeChild(dltbtn);
+    res.render("list",{
+        kindOfDay:day,newListItems:items
+    })
+    
 })
+app.post("/",function(req,res){
+    var item=req.body.newItem;
+    items.push(item);
+    res.redirect("/")
 })
+app.listen(3000,function(){
+    console.log("Server started on port 3000");
+})
+
+
+// let inputfield=document.getElementById('inputfield');
+// let addtodobtn=document.getElementById('addtodo');
+// let todocontainer=document.getElementById('todocontainer');
+
+// addtodobtn.addEventListener('click',function(){
+// var paragraph=document.createElement('p');
+// var dltbtn=document.createElement("button");
+// paragraph.innerText = inputfield.value;
+// dltbtn.innerText = "Delete";
+// todocontainer.appendChild(paragraph);
+// todocontainer.appendChild(dltbtn);
+// inputfield.value = "";
+
+
+
+// dltbtn.addEventListener('click', function(){
+//     todocontainer.removeChild(paragraph);
+//     todocontainer.removeChild(dltbtn);
+// })
+// })
